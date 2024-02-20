37cb4362be3bcdc48becec2cdfa2f6ac78929e08
Gabriel Esteves <gabriel.esteves@mail.com>
2024-02-20 12:05:52 -0300

    app funcional v1

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("App Funcional - v1"))
+		w.Write([]byte("App Funcional - v2"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
