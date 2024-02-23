0992ab97f2f0da1334be16f3eeae82693e0ab3be
Gabriel Esteves <gabriel.esteves@mail.com>
2024-02-23 10:31:11 -0300

    insert logs

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
+	// Define um logger para usar
+	logger := log.New(os.Stdout, "myapp ", log.LstdFlags|log.Lmicroseconds)
 
+	// Configura o handler para responder a todas as requisições HTTP com uma mensagem
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logger.Printf("Received request from %s\n", r.RemoteAddr)
 		w.Write([]byte("App Funcional - v1"))
 	})
-	http.ListenAndServe(":8080", nil)
+
+	// Inicia o servidor na porta 8080
+	logger.Println("Iniciando servidor na porta 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logger.Fatalf("Erro ao iniciar servidor: %s", err)
+	}
 }
